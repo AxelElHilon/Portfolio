07e1380d0a5dbb52a0d93d58a4f9557b25259cf3
Axel <axelelhilon@gmail.com>
2025-03-28 11:10:39 -0300

    Cambio de form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,30 @@
 // src/components/Contact.js
 import React from 'react';
 import './Contact.css';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'; // Importamos los íconos
 
 const Contact = () => {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
-      <form>
-        <input type="text" placeholder="Your Name" required />
-        <input type="email" placeholder="Your Email" required />
-        <textarea placeholder="Your Message" required></textarea>
-        <button type="submit">Send</button>
-      </form>
+      <p>If you'd like to reach out, you can contact me through any of the following:</p>
+      <ul>
+        <li className="contact-item">
+          <a href="mailto:axelelhilon@gmail.com" target="_blank" rel="noopener noreferrer">
+            <FaEnvelope className="contact-icon" />
+          </a>
+        </li>
+        <li className="contact-item">
+          <a href="https://github.com/AxelElHilon" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="contact-icon" />
+          </a>
+        </li>
+        <li className="contact-item">
+          <a href="https://www.linkedin.com/in/axel-el-hilon" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="contact-icon" />
+          </a>
+        </li>
+      </ul>
     </section>
   );
 };
